7675a73d3bd6bcc0cd846d2745290c58619d11d4
baz2222 <baz2222@i.ua>
2018-05-04 16:46:39 +0200

    working libjdx android on eclipse project

---
+++ core/src/com/juniorgames/spacestriker/SpaceStriker.java
@@ -0,0 +1,47 @@
+package com.juniorgames.spacestriker;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g3d.Environment;
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
+
+public class SpaceStriker extends ApplicationAdapter {
+	
+	public PerspectiveCamera cam;
+	public Model model;
+	public ModelInstance modelInstance;
+	public ModelBatch modelBatch;
+	public Environment environment;
+	public CameraInputController camController;
+	public AssetManager assets;
+	boolean loading;
+	float z_coord = 0f;
+	
+	@Override
+	public void create () {
+		
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
---
+++ desktop/src/com/juniorgames/spacestriker/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.juniorgames.spacestriker.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.juniorgames.spacestriker.SpaceStriker;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpaceStriker(), config);
+	}
+}
