7c7050e460530d7238ffe399df806971d1f1effe
baz2222 <baz2222@i.ua>
2018-05-07 16:51:15 +0200

    g3dj import working

--- core/src/com/juniorgames/spacestriker/SpaceStriker.java
+++ core/src/com/juniorgames/spacestriker/SpaceStriker.java
@@ -1,6 +1,7 @@
 package com.juniorgames.spacestriker;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Files.FileType;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
@@ -11,7 +12,9 @@ import com.badlogic.gdx.graphics.g3d.ModelBatch;
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.loader.G3dModelLoader;
 import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
+import com.badlogic.gdx.utils.UBJsonReader;
 
 public class SpaceStriker extends ApplicationAdapter {
 	
@@ -28,23 +31,25 @@ public class SpaceStriker extends ApplicationAdapter {
 	@Override
 	public void create () {
 		assets = new AssetManager();
-		assets.load("assets/default/models/model1/mainblock.obj",Model.class);
+		assets.load("assets/default/models/model1/model1.g3dj",Model.class);
 		loading = true;
 		modelBatch = new ModelBatch();
 		environment = new Environment();
 		environment.set(new ColorAttribute(ColorAttribute.AmbientLight,0.4f,0.4f,0.4f,1f));
 		environment.add(new DirectionalLight().set(0.8f,0.8f,0.8f,-1f,-0.8f,-0.2f));
 		cam = new PerspectiveCamera(67,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
-		cam.position.set(100f,100f,100f);
+		cam.position.set(0f,100f,100f);
 		cam.lookAt(0f, 0f, 0f);
 		cam.near = 1f;
 		cam.far = 300f;
 		cam.update();
 	}
 	public void doneLoading(){
-		model = assets.get("assets/default/models/model1/mainblock.obj",Model.class);
+		model = assets.get("assets/default/models/model1/model1.g3dj",Model.class);
 		modelInstance = new ModelInstance(model);
-		modelInstance.transform.setToTranslation(0,0,z_coord);
+		modelInstance.transform.rotate(1,0,0,-90);
+		modelInstance.transform.translate(0,0,-2);
+		//modelInstance.transform.setToTranslation(0,0,z_coord);
 		loading = false;
 	}
 	@Override
