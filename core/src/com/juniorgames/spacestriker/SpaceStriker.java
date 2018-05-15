706b0716ac8f727761672c1455c3608b5167cc22
baz2222 <baz2222@i.ua>
2018-05-15 15:59:50 +0200

    15-05-2018 g3dmodelloader and assetmanager works

--- core/src/com/juniorgames/spacestriker/SpaceStriker.java
+++ core/src/com/juniorgames/spacestriker/SpaceStriker.java
@@ -14,6 +14,7 @@ import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
 import com.badlogic.gdx.graphics.g3d.loader.G3dModelLoader;
 import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
+import com.badlogic.gdx.utils.JsonReader;
 import com.badlogic.gdx.utils.UBJsonReader;
 
 public class SpaceStriker extends ApplicationAdapter {
@@ -27,28 +28,49 @@ public class SpaceStriker extends ApplicationAdapter {
 	public AssetManager assets;
 	boolean loading;
 	float z_coord = 0f;
+	public G3dModelLoader modelLoader;
 	
 	@Override
 	public void create () {
+		modelsLoading();
+		camSetup();
+		envSetup();
+	}
+	public void modelsLoading(){
+		//G3DL();
+		AML();
+	}
+	public void G3DL(){//G3dModelloader using
+		modelBatch = new ModelBatch();
+		modelLoader = new G3dModelLoader(new JsonReader());
+		model = modelLoader.loadModel(Gdx.files.internal("assets/default/models/model1/MainBlock.g3dj"));
+		modelInstance = new ModelInstance(model);
+	}
+	public void AML(){//Asset manager using
 		assets = new AssetManager();
-		assets.load("assets/default/models/model1/model1.g3dj",Model.class);
+		assets.load("assets/default/models/model1/MainBlock.g3dj",Model.class);
 		loading = true;
 		modelBatch = new ModelBatch();
+		//System.out.println(assets.getProgress());
+	}
+	public void envSetup(){
 		environment = new Environment();
 		environment.set(new ColorAttribute(ColorAttribute.AmbientLight,0.4f,0.4f,0.4f,1f));
 		environment.add(new DirectionalLight().set(0.8f,0.8f,0.8f,-1f,-0.8f,-0.2f));
-		cam = new PerspectiveCamera(67,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
-		cam.position.set(0f,100f,100f);
+	}
+	public void camSetup(){
+		cam = new PerspectiveCamera(90,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+		cam.position.set(0f,0f,300f);
 		cam.lookAt(0f, 0f, 0f);
 		cam.near = 1f;
 		cam.far = 300f;
 		cam.update();
 	}
 	public void doneLoading(){
-		model = assets.get("assets/default/models/model1/model1.g3dj",Model.class);
+		model = assets.get("assets/default/models/model1/MainBlock.g3dj",Model.class);
 		modelInstance = new ModelInstance(model);
-		modelInstance.transform.rotate(1,0,0,-90);
-		modelInstance.transform.translate(0,0,-2);
+		//modelInstance.transform.rotate(1,0,0,-90);
+		//modelInstance.transform.translate(0,0,-2);
 		//modelInstance.transform.setToTranslation(0,0,z_coord);
 		loading = false;
 	}
