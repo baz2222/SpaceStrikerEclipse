c360203a7bd2ec7f47991ad85a4926a51c05855a
baz2222 <baz2222@i.ua>
2018-05-17 13:00:42 +0200

    display metrics problem with desktop module

---
+++ core/src/com/juniorgames/spacestriker/DeviceInfo.java
@@ -0,0 +1,11 @@
+package com.juniorgames.spacestriker;
+import android.util.DisplayMetrics;
+public final class DeviceInfo {
+	public static float deviceAspectRatio = 0.5859375f;
+private DeviceInfo()
+{}//constructor never runs
+public static void getDeviceInfo()
+{
+	//DisplayMetrics m = 
+}//getDeviceInfo
+}
